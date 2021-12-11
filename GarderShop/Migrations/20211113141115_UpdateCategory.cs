7454f7e4a2e0a97465927b56266723759ce43d7b
Zloy <qzloyp.gmail.com>
2021-12-11 14:28:10 +0900

    Добавленны компоненты; Добавленны контроллеры пользователя, продукта; Изменены контроллеры, представления

---
+++ GarderShop/Migrations/20211113141115_UpdateCategory.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GarderShop.Migrations
+{
+    public partial class UpdateCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryID",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryID",
+                table: "Categories",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_CategoryID",
+                table: "Categories",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_CategoryID",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_CategoryID",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryID",
+                table: "Categories");
+        }
+    }
+}
